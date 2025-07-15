a07d10f75a0e58a054e494947a3ffc6cfe118bfa
nicolefemello <nicfmello@gmail.com>
2025-07-15 14:21:54 -0300

    feat: creating a recovery page and refining styles and component reuse #7

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import HeaderComp from "@/components/general/HeaderComp.vue"
-import Forms from '@/components/auth/Forms.vue'
+import Form from '@/components/auth/Form.vue'
+import GoogleAuthButton from '@/components/auth/GoogleAuthButton.vue'
 
 const fields = [
   { id: 'name', label: 'Nome', placeholder: 'Digite seu nome aqui', type: 'text', autocomplete: 'name' },
   { id: 'email', label: 'Email', placeholder: 'Digite seu email aqui', type: 'email', autocomplete: 'email' },
-  { id: 'date', label: 'Data de nascimento', months: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"] },
+  { id: 'dateborn', label: 'Data de nascimento', name: 'Mês', options: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"] },
   { id: 'password', label: 'Senha', placeholder: 'Digite sua senha aqui', type: 'password', autocomplete: 'new-password' },
   { id: 'password-confirm', label: 'Confirme sua Senha', placeholder: 'Repita sua senha aqui', type: 'password', autocomplete: 'new-password' },
 ]
@@ -15,9 +16,10 @@ const fields = [
   <div class="grid justify-center">
     <HeaderComp title="Cadastro" />
 
-    <Forms :form-fields="fields" text="Continuar" />
+    <Form :form-fields="fields" text="Continuar" />
 
     <p class="text-center my-3">ou</p>
-    <button class="border border-[#2768CA] p-1 rounded-2xl text-sm">Continuar com o Google</button>
+    <GoogleAuthButton />
+    <!-- <button class="border border-[#2768CA] p-1 rounded-2xl text-sm">Continuar com o Google</button> -->
   </div>
 </template>
