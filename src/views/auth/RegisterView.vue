ab4b958a5eb6bc186bfd311a07660a69655752e9
matheusmichalski <matheus.michalskifc@gmail.com>
2025-07-15 15:30:33 -0300

    feat: enhance form component with TypeScript support and improve field definitions #7

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,14 +1,58 @@
 <script setup lang="ts">
-import HeaderComp from "@/components/general/HeaderComp.vue"
+import HeaderComp from '@/components/general/HeaderComp.vue'
 import Form from '@/components/auth/Form.vue'
 import GoogleAuthButton from '@/components/auth/GoogleAuthButton.vue'
+import type { FormField } from '@/types/forms'
 
-const fields = [
-  { id: 'name', label: 'Nome', placeholder: 'Digite seu nome aqui', type: 'text', autocomplete: 'name' },
-  { id: 'email', label: 'Email', placeholder: 'Digite seu email aqui', type: 'email', autocomplete: 'email' },
-  { id: 'dateborn', label: 'Data de nascimento', name: 'Mês', options: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"] },
-  { id: 'password', label: 'Senha', placeholder: 'Digite sua senha aqui', type: 'password', autocomplete: 'new-password' },
-  { id: 'password-confirm', label: 'Confirme sua Senha', placeholder: 'Repita sua senha aqui', type: 'password', autocomplete: 'new-password' },
+const fields: FormField[] = [
+  {
+    id: 'name',
+    label: 'Nome',
+    placeholder: 'Digite seu nome aqui',
+    type: 'text',
+    autocomplete: 'name',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    placeholder: 'Digite seu email aqui',
+    type: 'email',
+    autocomplete: 'email',
+  },
+  {
+    id: 'dateborn',
+    label: 'Data de nascimento',
+    type: 'date',
+    name: 'Mês',
+    options: [
+      'Janeiro',
+      'Fevereiro',
+      'Março',
+      'Abril',
+      'Maio',
+      'Junho',
+      'Julho',
+      'Agosto',
+      'Setembro',
+      'Outubro',
+      'Novembro',
+      'Dezembro',
+    ],
+  },
+  {
+    id: 'password',
+    label: 'Senha',
+    placeholder: 'Digite sua senha aqui',
+    type: 'password',
+    autocomplete: 'new-password',
+  },
+  {
+    id: 'password-confirm',
+    label: 'Confirme sua Senha',
+    placeholder: 'Repita sua senha aqui',
+    type: 'password',
+    autocomplete: 'new-password',
+  },
 ]
 </script>
 
@@ -16,7 +60,7 @@ const fields = [
   <div class="grid justify-center">
     <HeaderComp title="Cadastro" />
 
-    <Form :form-fields="fields" text="Continuar" />
+    <Form :form-fields="fields" button-text="Continuar" />
 
     <p class="text-center my-3">ou</p>
     <GoogleAuthButton />
