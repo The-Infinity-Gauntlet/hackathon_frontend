0899f3a3f82cb448fb95b55c977ddc0a95cc81d5
nicolefemello <nicfmello@gmail.com>
2025-08-05 16:45:40 -0300

    feat: building payment page #8

---
+++ src/modules/payment/controllers/PaymentController.ts
@@ -0,0 +1,131 @@
+import { computed, reactive } from 'vue'
+import { defineStore } from 'pinia'
+import { container } from 'tsyringe'
+
+import type { IPayment } from '../interfaces/payment'
+import { PaymentRepository } from '../repositories/PaymentRepository'
+
+export interface IPaymentControllerState {
+  payments: IPayment[]
+  currentPayment: Partial<IPayment>
+  pagination: {
+    page: number
+    pageSize: number
+    offset: number
+    limit: number
+  }
+  loading: boolean
+  search: string
+}
+
+export const usePaymentController = defineStore('payment', () => {
+  const paymentRepository = container.resolve(PaymentRepository)
+
+  const state = reactive<IPaymentControllerState>({
+    payments: [],
+    currentPayment: {},
+    pagination: {
+      page: 1,
+      pageSize: 100,
+      offset: 0,
+      limit: 10,
+    },
+    loading: false,
+    search: '',
+  })
+
+  const payments = computed(() => state.payments)
+  const currentPayment = computed(() => state.currentPayment)
+  const loading = computed(() => state.loading)
+
+  const pagination = computed({
+    get: () => state.pagination,
+    set: (value) => (state.pagination = value),
+  })
+
+  const search = computed({
+    get: () => state.search,
+    set: (value) => (state.search = value),
+  })
+
+  const getPayments = async () => {
+    state.loading = true
+    const result = await paymentRepository.list()
+    state.payments = result.results
+    state.pagination = {
+      ...state.pagination,
+      page: result.page_number,
+      page_size: result.page_size,
+      total: result.total,
+      num_pages: result.num_pages,
+    }
+    state.loading = false
+  }
+
+  const getPaymentById = async (id: string) => {
+    state.loading = true
+    const result = await paymentRepository.getById(id)
+    state.currentPayment = result
+    state.loading = false
+    return result
+  }
+
+  const clearCurrentPayment = () => {
+    state.currentPayment = {}
+  }
+
+  const createPayment = async (payment: Partial<IPayment>) => {
+    try {
+      console.log('Creating auth:', payment)
+
+      state.loading = true
+      const data = await paymentRepository.create(payment)
+      await getPayments()
+      return data
+    } catch (error) {
+      throw error
+    } finally {
+      state.loading = false
+    }
+  }
+
+  const updatePayment = async (id: string, payment: Partial<IPayment>) => {
+    try {
+      state.loading = true
+      const data = await paymentRepository.update(id, payment)
+      await getPayments()
+      return data
+    } catch (error) {
+      throw error
+    } finally {
+      state.loading = false
+    }
+  }
+
+  const deletePayment = async (id: string) => {
+    try {
+      state.loading = true
+      await paymentRepository.delete(id)
+      await getPayments()
+    } catch (error) {
+      throw error
+    } finally {
+      state.loading = false
+    }
+  }
+
+  return {
+    state,
+    payments,
+    currentPayment,
+    pagination,
+    loading,
+    search,
+    getPayments,
+    getPaymentById,
+    clearCurrentPayment,
+    createPayment,
+    updatePayment,
+    deletePayment,
+  }
+})
---
+++ src/modules/payment/interfaces/payment.ts
@@ -0,0 +1,7 @@
+export interface IPayment {
+  id: string
+  descricao?: string
+  createdAt?: string
+  updatedAt?: string
+  deletedAt?: string
+}
