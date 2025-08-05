433dea03d1c9a28fb3e2783f193fc0420d2f5d3d
nicolefemello <nicfmello@gmail.com>
2025-08-05 14:47:38 -0300

    chore: rename mobileMenu file #13

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import 'reflect-metadata'
 import headerComp from '@/@core/components/general/headerComp.vue'
-import Menu from './@core/components/general/menu.vue'
+import mobileMenu from './@core/components/general/mobileMenu.vue'
 </script>
 
 <template>
   <headerComp title="Aqua" />
   <RouterView />
-  <Menu class="fixed bottom-0" />
+  <mobileMenu class="fixed bottom-0" />
 </template>
