6cba7d0da88d2075debf4f27fd71a4a6be3e4ae1
nicolefemello <nicfmello@gmail.com>
2025-08-13 17:17:53 -0300

    feat: building profile pages #23

--- src/@templates/layouts/Default.vue
+++ src/@templates/layouts/Default.vue
@@ -6,20 +6,11 @@ import { HeaderComp, MobileMenu } from '@/@core/components'
   <div>
     <HeaderComp :title="$route.name" />
     <RouterView v-slot="{ Component }" class="mt-8">
-      <Transition
-        mode="out-in"
-        enter-active-class="transition duration-300 ease-out"
-        leave-active-class="transition duration-200 ease-in"
-        enter-from-class="opacity-0 translate-y-4"
-        enter-to-class="opacity-100 translate-y-0"
-        leave-from-class="opacity-100 translate-y-0"
-        leave-to-class="opacity-0 translate-y-4"
-      >
-        <component
-          :is="Component"
-          :key="$route.fullPath"
-          class="grid px-5 pb-20 sm:px-10 md:px-15 lg:px-20 xl:px-25"
-        />
+      <Transition mode="out-in" enter-active-class="transition duration-300 ease-out"
+        leave-active-class="transition duration-200 ease-in" enter-from-class="opacity-0 translate-y-4"
+        enter-to-class="opacity-100 translate-y-0" leave-from-class="opacity-100 translate-y-0"
+        leave-to-class="opacity-0 translate-y-4">
+        <component :is="Component" :key="$route.fullPath" class="grid px-5 pb-20 sm:px-10 md:px-15 lg:px-20 xl:px-25" />
       </Transition>
     </RouterView>
     <MobileMenu class="fixed bottom-0 lg:hidden" />
