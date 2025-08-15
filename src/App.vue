b49e3eabd243295833c5f70f8db3e779adfd5d2e
nicolefemello <nicfmello@gmail.com>
2025-08-14 22:20:49 -0300

    feat: building InAppNotification #23

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <script setup lang="ts">
 import 'reflect-metadata'
+
+import { onMounted } from 'vue'
+import { useInAppMessagingStore } from '@/@core/plugins/registered/pinia/inAppMessaging'
+import InAppMessages from '@/@core/components/InAppMessages.vue'
+
+const store = useInAppMessagingStore()
+
+onMounted(() => {
+  if (!sessionStorage.getItem('welcomeNotificationShown')) {
+    const id = Date.now().toString()
+
+    store.messages.push({
+      id,
+      text: 'Bem-vindo(a) ao site! 🎉'
+    })
+
+    sessionStorage.setItem('welcomeNotificationShown', 'true')
+
+    setTimeout(() => {
+      store.messages = store.messages.filter((m) => m.id !== id)
+    }, 5000)
+  }
+})
 </script>
 
 <template>
   <RouterView v-slot="{ Component }">
-    <Transition
-      mode="out-in"
-      enter-active-class="transition duration-300 ease-out"
-      leave-active-class="transition duration-200 ease-in"
-      enter-from-class="opacity-0 translate-y-4"
-      enter-to-class="opacity-100 translate-y-0"
-      leave-from-class="opacity-100 translate-y-0"
-      leave-to-class="opacity-0 translate-y-4"
-    >
-      <component :is="Component" :key="$route.fullPath" />
+    <Transition mode="out-in" enter-active-class="transition duration-300 ease-out"
+      leave-active-class="transition duration-200 ease-in" enter-from-class="opacity-0 translate-y-4"
+      enter-to-class="opacity-100 translate-y-0" leave-from-class="opacity-100 translate-y-0"
+      leave-to-class="opacity-0 translate-y-4">
+      <div>
+        <InAppMessages />
+        <component :is="Component" :key="$route.fullPath" />
+      </div>
     </Transition>
   </RouterView>
 </template>
