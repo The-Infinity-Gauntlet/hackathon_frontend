bdd01291ff776a114531f800bd9c883f831c6ae4
nicolefemello <nicfmello@gmail.com>
2025-08-22 17:19:30 -0300

    feat: building home admin page #33

--- src/@core/components/carousels/menuItems.vue
+++ src/@core/components/carousels/menuItems.vue
@@ -5,10 +5,8 @@ defineProps<{
 </script>
 
 <template>
-  <div
-    :class="item.id % 2 == 0 ? 'bg-[#2768CA] text-white' : 'bg-[#E5E5E5] text-[#999999]'"
-    class="grid h-[160px] w-[140px] rounded-2xl p-3 text-center text-sm lg:h-[200px] lg:w-[180px] lg:rounded-md"
-  >
+  <div :class="item.id % 2 == 0 ? 'bg-[#2768CA] text-white' : 'bg-[#E5E5E5] text-[#999999]'"
+    class="grid h-[160px] w-[140px] rounded-2xl p-3 text-center text-sm lg:h-[200px] lg:w-[180px] lg:rounded-md">
     <RouterLink :to="item.link" class="grid w-full grid-rows-[1fr_auto] gap-3">
       <div class="flex items-center justify-center">
         <p class="font-semibold lg:text-lg">{{ item.label }}</p>
