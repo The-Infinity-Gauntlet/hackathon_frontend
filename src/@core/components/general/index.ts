41ec327a132c755bc561fe0090a96309fe864b42
nicolefemello <nicfmello@gmail.com>
2025-09-15 14:47:40 -0300

    feat: building loading component #37

--- src/@core/components/general/index.ts
+++ src/@core/components/general/index.ts
@@ -1,4 +1,5 @@
 export { default as HeaderComp } from './headerComp.vue'
+export { default as Loading } from './loading.vue'
 export { default as InAppMessages } from './inAppMessages.vue'
 export { default as MobileMenu } from './mobileMenu.vue'
 export { default as ThemeSwitcher } from './themeSwitcher.vue'
