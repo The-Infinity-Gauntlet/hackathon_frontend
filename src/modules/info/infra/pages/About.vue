25bb50c56ad8b83474624ddadff40d8b69aa6096
rafael-otto <rafael.august.otto.ifc@gmail.com>
2025-09-21 09:54:14 -0300

    style: adjust responsiveness and replace paragraph with unordered list

--- src/modules/info/infra/pages/About.vue
+++ src/modules/info/infra/pages/About.vue
@@ -3,32 +3,41 @@
 <template>
   <div class="grid gap-8 px-5 sm:px-10 md:px-15 lg:px-20 xl:px-25">
     <h1 class="mb-10 hidden text-center text-2xl font-semibold lg:block">Sobre nós</h1>
-    <section class="my-7 grid gap-5">
+    <section class="my-4 grid gap-5">
       <h2 class="text-center font-semibold lg:text-2xl">A nossa história</h2>
       <img
         src=""
         alt="História"
-        class="w-full rounded-2xl bg-[#D9D9D9] object-cover lg:mx-auto lg:w-[70%]"
+        class="w-full mx-auto rounded-2xl bg-[#D9D9D9] object-cover lg:w-[40%]"
       />
-      <p>
+      <p class="text-justify">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, corporis! Mollitia pariatur,
         ducimus voluptate tempora provident, corporis maiores veritatis quidem illo excepturi
         officia beatae suscipit nulla assumenda consequatur debitis incidunt.
       </p>
     </section>
 
-    <section class="my-7 grid gap-5">
+    <section class="my-4 grid gap-5">
       <h2 class="text-center font-semibold lg:text-2xl">Nossos objetivos</h2>
       <img
         src=""
         alt="Objetivos"
-        class="w-full rounded-2xl bg-[#D9D9D9] object-cover lg:mx-auto lg:w-[70%]"
+        class="w-full mx-auto rounded-2xl bg-[#D9D9D9] object-cover lg:w-[40%]"
       />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, corporis! Mollitia pariatur,
-        ducimus voluptate tempora provident, corporis maiores veritatis quidem illo excepturi
-        officia beatae suscipit nulla assumenda consequatur debitis incidunt.
-      </p>
+      <ul class="text-justify list-disc mx-5">
+        <li class="my-2">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </li>
+        <li class="my-2">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </li>
+        <li class="my-2">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </li>
+        <li class="my-2">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
